d75c367ec3e2ff83b7c2510bb7ede4879e231eb6
randrianandrasanahery <randrianandrasanaherytanjona@gmail.com>
2023-05-19 11:16:22 +0300

    commit second

---
+++ src/script/index.js
@@ -0,0 +1,14 @@
+var lien_competence = document.querySelector("active_link");
+var lien_education = document.querySelector("education");
+var lien_experience = document.querySelector("experience");
+var placement_content = document.querySelector("link_contente_skills");
+
+function education_content(){
+   if (lien_education == true){
+      placement_content.textContent = "bonjour enao"
+   }
+ 
+}
+
+
+
